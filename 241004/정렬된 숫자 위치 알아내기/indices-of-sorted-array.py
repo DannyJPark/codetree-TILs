cb4886bad971821cfec2a59c5f64af85e6775938
DannyPark <138744012+DannyJPark@users.noreply.github.com>
2024-10-04 18:23:46 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 241004/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 241004/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -1,12 +1,12 @@
-n = int(input())  
+n = int(input())
 sequence = list(map(int, input().split()))
 
 indexed_sequence = [(value, index) for index, value in enumerate(sequence)]
+
 indexed_sequence.sort(key=lambda x: x[0])
 
 result = [0] * n
 
 for new_index, (value, original_index) in enumerate(indexed_sequence):
-    result[original_index] = new_index
-
+    result[original_index] = new_index + 1 
 print(" ".join(map(str, result)))
